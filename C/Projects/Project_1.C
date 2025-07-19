362c3e5a90db06af4a17782ac32dbafcb6a77abc
coder-ravi100 <ravirathod100100@gmail.com>
2025-07-19 18:39:48 +0530

    update

--- C/Projects/Project_1.C
+++ C/Projects/Project_1.C
@@ -2,6 +2,52 @@
 #include<stdio.h>
 int main()
 {
-    int num1,num2;
+   
+    float num1 = 20 ,num2 = 30, ans;
+    int choice;
+    do
+    {
+    printf("\n =========================");
+    printf("\n----Mini Calculator----");
+    printf("\n 1.Addition");
+    printf("\n 2.Subtraction");
+    printf("\n 3.Multiplication");
+    printf("\n 4.Division");
+    printf("\n 5.Exit");
+    printf("\n Enter Your Choice :");
+    scanf("%d",&choice);
+    printf("\n =========================");
+    
+    switch (choice)
+    {
+        case 1:
+                printf("\nResult %.2f ",ans = num1+num2);
+        break;
+
+        case 2:
+                printf("\nResult %.2f ",ans = num1-num2);
+        break;
+
+        case 3:
+                printf("\nResult %.2f ",ans = num1*num2);
+        break;
+
+        case 4:
+                printf("\nResult %.2f ",ans = num1/num2);
+        break;
+
+        case 5:
+               printf("\nExiting Calculator. Thank you!");  
+        break;
+
+         default:
+         {
+             printf("\nInvalid Choice! Please try again.");
+         }
+    }
+    
+
+    } while (choice != 5);
+    
     return 0;
 }
